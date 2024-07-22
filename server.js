0cf6a3a9f423322286a76333c3bbcdcfd14ca239
Azmat Alam <azmat5star@gmail.com>
2024-07-22 18:40:14 +0530

    first

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const request = require('request');
+const cors = require('cors');
+const app = express();
+require('dotenv').config(); 
+
+app.use(cors());
+
+const port = process.env.PORT;
+
+app.use('/proxy', (req, res) => {
+  const url = req.query.url;
+  req.pipe(request(url)).pipe(res);
+});
+
+app.listen(3002, () => {
+  console.log(`Proxy server running${port}`);
+});
